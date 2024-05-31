0c65d61c44c8b6c104565cce544f2b2175b46223
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2024-05-31 10:41:17 +0530

    Update addmission.php

--- addmission.php
+++ addmission.php
@@ -4,7 +4,6 @@
 <script src="js/gmaps.min.js"></script>
 <!-- Map Active JS -->
 <?php include 'header.php' ?>
-
 <!-- Breadcrumbs -->
 <div class="breadcrumbs overlay">
 	<div class="container">
@@ -23,90 +22,16 @@
 	</div>
 </div>
 <!-- End Breadcrumbs -->
-
 <!-- Table -->
 <section>
 	<div class="container">
 		<div class="row mt-5">
-			<div class="col-md-6 p-2">
-				<div>
-					<h5 style="text-decoration: underline; font-weight:600;">Courses offered</h5>
-					<p style="color: #212121; font-size: 16px; line-height: 2rem;" class="mt-4">
-						At the + 2 levels <br>
-						<span style="font-weight: 700;">Science</span><br>
-						English, Physics, Chemistry, Maths/Biology.
-						<br>
-						<span style="font-weight: 700;">Commerce</span><br>
-						English, Accountancy, Economics, Business Studies.
-						<br>
-						<span style="font-weight: 700;">Humanities</span><br>
-						History, Political Science, Geography
-						<br>
-						<span style="font-weight: 700;">Optional Subjects</span><br>
-						Computer Science, Physical Education, Maths, Bio.
-					</p>
-				</div>
-
-				<div class="mt-5">
-					<h5 style="text-decoration: underline; font-weight:600;">Age Criteria</h5>
-					<table style="border: 1px solid #1baaec;" class="mt-4">
-						<tbody>
-							<tr style="border: 1px solid #1baaec; color: #212121; text-align: center; font-size: 16px;">
-								<td style="background:#C7E9F9; color: #213d75; font-weight: 600;" class="py-2">
-									NURSURY</td>
-								<td>3 to 4 year</td>
-							</tr>
-							<tr style="border: 1px solid #1baaec; color: #212121; text-align: center; font-size: 16px;">
-								<td style="background:#C7E9F9; color: #213d75; font-weight: 600;" class="py-2">
-									JUNIOR KG</td>
-								<td>4 to 5 year</td>
-							</tr>
-							<tr style="border: 1px solid #1baaec; color: #212121; text-align: center; font-size: 16px;">
-								<td style="background:#C7E9F9; color: #213d75; font-weight: 600;" class="py-2">
-									SENIOR KG</td>
-								<td>5 to 6 year</td>
-							</tr>
-							<tr style="border: 1px solid #1baaec; color: #212121; text-align: center; font-size: 16px;">
-								<td style="background:#C7E9F9; color: #213d75; font-weight: 600;" class="py-2">CLASS
-									1</td>
-								<td>6 to 7 year</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-
-				<div class="my-5">
-					<div class="row">
-						<div class="col-md-6">
-							<img src="img/brochures/brochuresImgBtn.png">
-						</div>
-						<div class="col-md-6">
-							<img src="img/brochures/brochuresImgBtn.png">
-						</div>
-					</div>
-
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div>
-					<h5 style="text-decoration: underline; font-weight:600;">Important Information</h5>
-					<div style="background: #fbba00;" class="px-2 py-4 mt-2">
-						<h5 style="text-decoration: underline; font-weight:600;" class="my-3">Courses offered</h5>
-						<ol style="color: #212121; line-height: 2rem; padding: 15px 25px;">
-							<li>Self attested photo copies of the marks records of previous years (if applicable).</li>
-							<li>One certified/self attested photo copy of the child's birth certificate with proper name and other details written correctly.</li>
-							<li>Original transfer certificate from the last school attended (if any).</li>
-							<li>2 passport size photos with name and date of photograph clicked of the child and the parents (both).</li>
-							<li>Address proof (telephone bill, electricity bill/Ration card/driving license).</li>
-							<li>Admission fee/registration fee in cash/cheque in favour of school.</li>
-							<li>Medical certificate if any specific ailment on deformity.</li>
-						</ol>
-					</div>
-				</div>
-			</div>
+			<iframe
+				src="https://docs.google.com/forms/d/e/1FAIpQLScGw_usXBN4EJr_LcPCTXO8V-2bzNpNkDk5ESuJ6g6VoIeffQ/viewform"
+				style="border:0px #ffffff none;" name="myiFrame" scrolling="yes" frameborder="1" marginheight="0px"
+				marginwidth="0px" height="750px" width="100%" allowfullscreen></iframe>
 		</div>
 	</div>
 </section>
 <!-- Table -->
-
 <?php include 'footer.php' ?>
